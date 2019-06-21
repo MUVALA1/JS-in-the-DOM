d271a3cd74ee75a5e6fff125676234d928272df6
nyiramirembe <peninahhot@gmail.com>
2019-06-21 14:00:45 +0300

    changes

---
+++ script.js
@@ -0,0 +1,4 @@
+let button=document.getElementById("changeBackground");
+//assigning button to access changebackground thing
+
+button.addEventListener("click",()=>{document.body.style.backgroundColor = "fuchsia";});
